55933361cee6eaff5a4f8afe58d2574248b86f6c
Jose Miguel Ochoa <josemiguelo.ochoa@gmail.com>
2025-12-22 18:52:01 -0500

    copy all assets

--- scripts/fedora.sh
+++ scripts/fedora.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # Symlink distrobox shims
-./set-distrobox-shims.sh
+/tmp/scripts/set-distrobox-shims.sh
 
 # Update the container and install packages
 dnf5 update -y
-grep -v '^#' ./fedora.packages | xargs dnf5 install -y
+grep -v '^#' /tmp/packages/fedora.packages | xargs dnf5 install -y
 
 # set asdf
-./set-asdf.sh
+/tmp/scripts/set-asdf.sh
