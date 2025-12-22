ae5ff0cbcfd35c2115f5df5ab0c59405b2afd800
Jose Miguel Ochoa <josemiguelo.ochoa@gmail.com>
2025-12-22 17:12:00 -0500

    kick start fedora boxkit

--- scripts/fedora.sh
+++ scripts/fedora.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Symlink distrobox shims
-./distrobox-shims.sh
+./set-distrobox-shims.sh
 
 # Update the container and install packages
 dnf update -y
-grep -v '^#' ./fedora-example.packages | xargs dnf install -y
+grep -v '^#' ./fedora.packages | xargs dnf install -y
