9d2feba1cb99aded0f1646dc5327f9636939e5aa
Jose Miguel Ochoa <josemiguelo.ochoa@gmail.com>
2025-12-26 19:53:30 -0500

    export wezterm as an app to the host

--- scripts/install-dnf-packages.sh
+++ scripts/install-dnf-packages.sh
@@ -68,6 +68,7 @@ dnf5 install -y yazi
 ## WEZTERM ##
 #############
 dnf5 install -y https://github.com/wezterm/wezterm/releases/download/20240203-110809-5046fc22/wezterm-20240203_110809_5046fc22-1.fedora39.x86_64.rpm
+distrobox-export --app wezterm
 
 ## clean everything ......
 dnf5 clean all
