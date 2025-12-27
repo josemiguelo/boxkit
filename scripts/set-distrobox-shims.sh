86479f970a2c710793d4f71e24deee6297d1e15b
Jose Miguel Ochoa <josemiguelo.ochoa@gmail.com>
2025-12-26 19:12:42 -0500

    remove some host-exec apps

--- scripts/set-distrobox-shims.sh
+++ scripts/set-distrobox-shims.sh
@@ -2,9 +2,5 @@
 
 set -ouex pipefail
 
-[ ! -e /usr/bin/sh ] && ln -fs /bin/sh /usr/bin/sh
 ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/docker
-ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/flatpak
 ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/podman
-ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/rpm-ostree
-ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/transactional-update
