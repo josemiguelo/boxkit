d772bef8b89696c4e659430a6ac0f56c8b74bd06
Jose Miguel Ochoa <josemiguelo.ochoa@gmail.com>
2025-12-26 21:59:31 -0500

    link distrobox host-exec

--- scripts/set-distrobox-shims.sh
+++ scripts/set-distrobox-shims.sh
@@ -4,3 +4,4 @@ set -ouex pipefail
 
 ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/docker
 ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/podman
+ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/distrobox
