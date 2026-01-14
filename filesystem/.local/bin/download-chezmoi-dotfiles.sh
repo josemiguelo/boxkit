c6a51910d211fbb0ef9be695f2a63f870cbb8113
Jose Miguel Ochoa <josemiguelo.ochoa@gmail.com>
2026-01-14 17:17:59 -0500

    set a default wezterm theme after installation

--- filesystem/.local/bin/download-chezmoi-dotfiles.sh
+++ filesystem/.local/bin/download-chezmoi-dotfiles.sh
@@ -6,7 +6,8 @@ if [ ! -d "$HOME/.local/share/chezmoi" ]; then
   echo "🚀 Initializing dotfiles in isolated home: $HOME"
   sudo chown -R $USER:$USER "$HOME/.local"
   chezmoi init --apply --verbose --force https://github.com/josemiguelo/.dotfiles.git
+
+  echo "return { color_scheme = 'Tokyo Night Day' }" >"$HOME/.config/wezterm/private.lua"
 else
   echo "🚀 Dotfiles have already been initialized!"
 fi
-
