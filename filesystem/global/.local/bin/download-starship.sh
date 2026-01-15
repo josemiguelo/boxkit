c7e973728083e313f404b3c1944a4e59065e54c2
Jose Miguel Ochoa <josemiguelo.ochoa@gmail.com>
2026-01-14 23:24:40 -0500

    add checks to new bins

--- filesystem/global/.local/bin/download-starship.sh
+++ filesystem/global/.local/bin/download-starship.sh
@@ -6,5 +6,9 @@ SHELL_TYPE="zsh"
 BIN_DIR="$HOME/.local/bin"
 
 echo "Starting Starship installation..."
-curl -sS https://starship.rs/install.sh | sh -s -- -y -b "$BIN_DIR"
+if ! command -v starship &> /dev/null; then
+    curl -sS https://starship.rs/install.sh | sh -s -- -y -b "$BIN_DIR"
+else
+    echo "Starship is already installed, skipping download."
+fi
 echo "Finishing Starship installation..."
--- filesystem/global/.local/bin/setup-chezmoi.sh
+++ filesystem/global/.local/bin/setup-chezmoi.sh
@@ -2,4 +2,10 @@
 
 set -ouex pipefail
 
-sh -c "$(curl -fsLS get.chezmoi.io)" -- -b $HOME/.local/bin
+if ! command -v chezmoi &> /dev/null; then
+    echo "Chezmoi is not installed, downloading..."
+    sh -c "$(curl -fsLS get.chezmoi.io)" -- -b "$HOME/.local/bin"
+    echo "Chezmoi downloaded successfully."
+else
+    echo "Chezmoi is already installed, skipping download."
+fi
