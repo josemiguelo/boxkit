6c98c2f177059db701f26526962e7c5bb5d63eac
Jose Miguel Ochoa <josemiguelo.ochoa@gmail.com>
2026-02-03 17:32:27 -0500

    export bluebuild

--- scripts/fedora-dev/set-distrobox-shims.sh
+++ scripts/fedora-dev/set-distrobox-shims.sh
@@ -6,3 +6,4 @@ ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/docker
 ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/podman
 ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/distrobox
 ln -sf /usr/bin/distrobox-host-exec /usr/local/bin/xdg-open
+ln -sf /usr/bin/distrobox-host-exec /usr/local/bin/bluebuild
